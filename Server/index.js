ddc722393629dd7ad690895a37581e310a6e03aa
Muhammad-Owais-Warsi <warsimuhammadowais@gmail.com>
2024-09-09 11:02:11 +0530

    change

--- Server/index.js
+++ Server/index.js
@@ -1,24 +1,29 @@
 // server.js
 import express from 'express';
 import { AccessToken } from "livekit-server-sdk";
+import { RoomServiceClient, Room } from 'livekit-server-sdk';
 import cors from 'cors';
 var totalUsers = [];
 
 const app = express();
 app.use(cors());
 const port = 3000;
+
+const livekitHost = 'wss://test-et22r9u8.livekit.cloud';
+const roomService = new RoomServiceClient(livekitHost, 'APISR9G848vteWP', 'RcaPTBbC1ee5nDURfHmcOmtSy6SZHdT7De5ddn3cDzeC');
   
 const createToken = async () => {
     let roomName = 'quickstart-room';
   // If this room doesn't exist, it'll be automatically created when the first
   // client joins
-  if (totalUsers.length >= 2) {
-    roomName = 'quickstart-room2';
-  }
+  // if (totalUsers.length >= 2) {
+  //   roomName = 'quickstart-room2';
+  // }
   
-
-  
+  roomName = createRoom(roomName);
+  console.log(roomName);
   // Identifier to be used for participant.
+  
     
   const participantName = "hello"+Math.floor(Math.random() * (100 - 1 + 1)) + 1;
 
@@ -35,10 +40,16 @@ const createToken = async () => {
 
 const createRoom = async (name) => {
   const opts = {
-    name: name+Math.floor(Math.random() * (100 - 1 + 1)) + 1,
-    emptyTimeout: 10 * 60, // 10 minutes
+    name: name,
+    emptyTimeout: 10 , 
     maxParticipants: 20,
-  }
+  };
+  const room = await roomService.createRoom(opts);
+  console.log('Room created', room); // Log inside to confirm room is created
+  return room;
+
+
+ 
 };
 
 
